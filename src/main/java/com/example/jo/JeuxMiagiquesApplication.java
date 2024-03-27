a864b6f23f870b1eb6345b91c175d3824f5bd4d5
ArnaudMourgues <arnaud.mourgues@yahoo.fr>
2024-03-27 09:39:30 +0100

    init

---
+++ src/main/java/com/example/jo/JeuxMiagiquesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxMiagiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuxMiagiquesApplication.class, args);
+	}
+
+}
